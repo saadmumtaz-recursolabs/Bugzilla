b5cf9be92b750ef688cac137fcdf9ad6ce096d38
saadmumtaz-recursolabs <saad.mumtaz@recursolabs.com>
2025-04-09 19:38:21 +0500

    Added database connection code

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./server/config/db');
+const cors = require('cors');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
